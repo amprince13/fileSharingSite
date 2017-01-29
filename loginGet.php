639b4783aea6d1db0f1a9eec1873f747e2ae6057
Austin Prince <amprince@wustl.edu>
2017-01-28 22:43:04 -0600

    Fixed entry page and links to users page

--- loginGet.php
+++ loginGet.php
@@ -3,21 +3,21 @@
 	session_start();
 	$userName = (String)$_GET["userName"];
 	$readFile = fopen("user.txt", "r");
-
 	$lineNum = 1;
 	// check if user name was entered
 	if(isset($_GET["login"]) && !empty($userName)){
 		while(!feof($readFile)){
-			$readLine = (String)fgets($readFile);
-			echo $readLine . "/n";
-			echo $userName;
-			if (trim($userName) == $readLine){
+			$readLine = fgets($readFile);
+			if (trim($userName) == trim($readLine)){
 				$_SESSION["userName"] = $userName;
 				echo "You are logging";
+				header ("Location:fileMain.php");
+				exit;
 			}
 			$lineNum++;
 		}
 	echo "User not found";
+	fclose($readFile);
 
 	}
 
